4a4edb5ee34c9650b8ea3ad596edb8e96b8d93c0
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-04-22 11:34:39 +0300

    show modal window only if other have display: none

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,10 +4,10 @@ import closePopups from "./closePopups";
 const modals = () => {
 	function bindModals(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 		const triggers = document.querySelectorAll(triggerSelector),
-				modal = document.querySelector(modalSelector),
-				close = document.querySelector(closeSelector),
-				scrollWidth = calcScroll();
-		
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector),
+			scrollWidth = calcScroll();
+
 		triggers.forEach(item => {
 			item.addEventListener("click", (e) => {
 				if (e.target) {
@@ -15,7 +15,7 @@ const modals = () => {
 				}
 
 				closePopups();
-				
+
 				modal.style.display = "block";
 				document.body.style.overflow = "hidden";
 				document.body.style.marginRight = `${scrollWidth}px`;
@@ -26,7 +26,7 @@ const modals = () => {
 			closePopups();
 			modal.style.display = "none";
 		});
-		
+
 		modal.addEventListener("click", (e) => {
 			if (e.target === modal && closeClickOverlay) {
 				closePopups();
@@ -37,9 +37,18 @@ const modals = () => {
 
 	function showModalByTime(selector, time) {
 		setTimeout(() => {
-			document.querySelector(selector).style.display = "block";
-			document.body.style.overflow = "hidden";
-			document.body.style.marginRight = `${scrollWidth}px`;
+			let isDisplay;
+			document.querySelectorAll("[data-modal").forEach(item => {
+				if (getComputedStyle(item).display != "none") {
+					isDisplay = true;
+				}
+			});
+			
+			if (!isDisplay) {
+				document.querySelector(selector).style.display = "block";
+				document.body.style.overflow = "hidden";
+				document.body.style.marginRight = `${scrollWidth}px`;
+			}
 		}, time);
 	}
 
@@ -48,8 +57,7 @@ const modals = () => {
 	bindModals(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
 	bindModals(".popup_calc_button", ".popup_calc_profile", ".popup_calc_profile_close", false);
 	bindModals(".popup_calc_profile_button", ".popup_calc_end", ".popup_calc_end_close", false);
-	// showModalByTime(".popup[data-modal]", 6000);
-
+	showModalByTime(".popup[data-modal]", 60000);
 };
 
 export default modals;
